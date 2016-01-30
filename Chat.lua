d69ab83fb3ca43718d93633c71455d04d6e5131e
MOSAVI17 <www.mosavi7857@gmail.com>
2016-01-30 16:56:07 +0330

    Create Chat.lua

---
+++ Chat.lua
@@ -0,0 +1,69 @@
+local function run(msg)
+if msg.text == "hi" then
+	return "Hello bb"
+end
+if msg.text == "Hi" then
+	return "Hello honey"
+end
+if msg.text == "Hello" then
+	return "Hi bb"
+end
+if msg.text == "hello" then
+	return "Hi honey"
+end
+if msg.text == "Salam" then
+	return "Salam aleykom"
+end
+if msg.text == "salam" then
+	return "va aleykol asalam"
+end
+if msg.text == "IRANTG" then
+	return "Bot Anti Spaming..."
+end
+if msg.text == "irantg" then
+	return "Bot Anti Spaming..."
+end
+if msg.text == "IranTG" then
+	return "Bot Anti Spaming Telegram..."
+end
+if msg.text == "Mr.exe" then
+	return "His my Father:-)) "
+end
+if msg.text == "Mrexe" then
+	return "His my Father"
+end
+if msg.text == "bot" then
+	return "Bale?"
+end
+if msg.text == "Bot" then
+	return "Huuuum?"
+end
+if msg.text == "?" then
+	return "Hum??"
+end
+if msg.text == "Bye" then
+	return "Bay Bay"
+end
+if msg.text == "bye" then
+	return "Bye Bye"
+end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^[Hh]i$",
+		"^[Hh]ello$",
+		"^[Zz]ac$",
+		"^ZAC$",
+		"^[Bb]ot$",
+		"^[Uu]mbrella$",
+		"^[Bb]ye$",
+		"^?$",
+		"^[Ss]alam$",
+		}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
